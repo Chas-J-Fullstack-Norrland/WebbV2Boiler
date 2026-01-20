0bb825c2acf8fe9d31a9e637ca1971990cebe8e2
Erik Berglund <144246010+Svartakatten@users.noreply.github.com>
2026-01-20 12:40:59 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: 'app', // Detta är magin. Vite kommer tro att "app"-mappen är roten.
+  root: 'app',
   build: {
-    outDir: '../dist', // Vi vill fortfarande att bygget ska hamna utanför 'app'
+    outDir: '../dist',
     emptyOutDir: true,
   },
-  publicDir: 'public' // Vite letar nu efter public inuti 'app/public' automatiskt
+  publicDir: 'public'
 });
