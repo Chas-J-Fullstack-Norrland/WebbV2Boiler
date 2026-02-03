9f38f11e0af7e3eb931f7ad067e2c4ecc91de4f1
Joso1207 <joso1207@student.miun.se>
2026-02-03 10:28:42 +0100

    TypeChanges

--- src/main.ts
+++ src/main.ts
@@ -5,18 +5,20 @@ import {
 } from './api';
 
 // --- TYPER ---
-interface Checklist {
+export interface Post {
   id: string;
   title: string;
   author: string;
+  content: string;
   date: string;
+  published?: boolean
 }
 
-interface Task {
+export interface Comment {
   id: string;
-  checklistId: string;
+  postid: string;
   text: string;
-  completed: boolean;
+  author: string;
 }
 
 // --- ROUTING ---
