564d821d04d06d9280ab99206e3217ee5d1443be
Joso1207 <joso1207@student.miun.se>
2026-02-03 10:30:56 +0100

    branching

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,127 @@
 // src/api.ts
 const BASE_URL = '/api';
+import type {Post,Comment} from "./main"
+
+
+export async function getPosts() {
+  try {
+    const response = await fetch(`${BASE_URL}/posts`);
+    const data:Post[] = await response.json();
+    
+    localStorage.setItem("posts",JSON.stringify(data));
+    if (!response.ok) {
+      throw new Error(`API error: ${response.status}`);
+    }
+;
+    return data;
+
+  } catch (error) {
+    const cached: Post[] = JSON.parse(
+      localStorage.getItem("posts") ?? "[]"
+    );
+    console.error("Failed to fetch posts:", error);
+
+    return cached;
+  }
+}
+
+export async function getComments() {
+  try {
+    const response = await fetch(`${BASE_URL}/comments`);
+    const data:Comment[] = await response.json();
+    
+    localStorage.setItem("comments",JSON.stringify(data));
+    if (!response.ok) {
+      throw new Error(`API error: ${response.status}`);
+    }
+;
+    return data;
+
+  } catch (error) {
+    const cached: Comment[] = JSON.parse(
+      localStorage.getItem("comments") ?? "[]"
+    );
+    console.error("Failed to fetch posts:", error);
+
+    return cached;
+  }
+}
+
+export async function getPost(id:Number) {
+  try {
+    const response = await fetch(`${BASE_URL}/posts/${id}`);
+
+    if (!response.ok) {
+      throw new Error(`API error: ${response.status}`);
+    }
+
+    const todo = await response.json();
+    return todo;
+
+  } catch (error) {
+    console.error("Failed to fetch post:", error);
+
+    throw error;
+  }
+}
+
+export async function getComment(id:Number) {
+  try {
+    const response = await fetch(`${BASE_URL}/comments/${id}`);
+
+    if (!response.ok) {
+      throw new Error(`API error: ${response.status}`);
+    }
+
+    const todo = await response.json();
+    return todo;
+
+  } catch (error) {
+    console.error("Failed to fetch comment:", error);
+
+    throw error;
+  }
+}
+
+const POST_QUEUE_KEY = "post-queue";
+
+
+function getPostQueue(): [] {
+  const stored = localStorage.getItem(POST_QUEUE_KEY);
+  return stored ? (JSON.parse(stored) as []) : [];
+}
+
+function savePostQueue(queue: []): void {
+  localStorage.setItem(POST_QUEUE_KEY, JSON.stringify(queue));
+}
+
+export async function addPost(post:Post) {
+  
+  try{
+    const response = await fetch(`${BASE_URL}/posts/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(todo)
+  });
+  
+  if (!response.ok) {
+    throw new Error('Failed to add post');
+  }
+
+  return response.json();
+  }
+  catch(error){
+    const queue = getPostQueue();
+    queue.push(todo);
+    savePostQueue(queue);
+  }
+}
+
+
+
+
 
 /** --- CHECKLISTOR (Huvudlistor) --- **/
 
