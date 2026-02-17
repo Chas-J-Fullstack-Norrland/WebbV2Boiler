ee64c1a19f52a8da621c51b0a96b31fd5055e8dd
Joso1207 <joso1207@student.miun.se>
2026-02-17 10:33:03 +0100

    github workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/webb2-boiler/' : '/',
   plugins: [
     tailwindcss(),
     VitePWA({
