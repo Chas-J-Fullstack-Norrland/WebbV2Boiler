4c4a7acd5fd25b3cec8320f854503ab6a65d0677
Joso1207 <joso1207@student.miun.se>
2026-02-17 12:09:47 +0100

    service worker needs relational reference to file.

--- src/main.ts
+++ src/main.ts
@@ -72,7 +72,7 @@ window.addEventListener('online', async () => {
 
 if ('serviceWorker' in navigator  && import.meta.env.PROD) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('./sw.js')
       .then((registration) => {
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
       })
