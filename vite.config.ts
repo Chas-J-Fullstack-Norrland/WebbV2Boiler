a4a891cbefafcb0087e5fdaf631a87e3f50b6f05
Joso1207 <joso1207@student.miun.se>
2026-02-17 11:37:24 +0100

    Base needs to mention the actual url part

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/webb2-boiler/' : '/',
+  base: mode === 'production' ? '/WebbV2Boiler/' : '/',
   plugins: [
     tailwindcss(),
     VitePWA({
